41dff693ec44003b7213f347d62b0ec8b1459ab1
Lane Albrecht <ltalbrec@ucsc.edu>
2021-04-05 16:42:32 -0700

    initial commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+//const { Phaser } = require("../lib/phaser.min");
+
+// game config
+let config = {
+    type: Phaser.CANVAS,
+    width: 640,
+    height: 480
+}
+
+let game = new Phaser.Game(config);
